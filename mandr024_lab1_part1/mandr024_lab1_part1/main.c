f1a3b49aa3d4afd831e8cf10a60996727ac31f58
miguelandrade4 <miguel.jr.andrade49@gmail.com>
2018-07-30 14:45:47 -0700

    Problem 1 solution
    if else statements used.

--- mandr024_lab1_part1/mandr024_lab1_part1/main.c
+++ mandr024_lab1_part1/mandr024_lab1_part1/main.c
@@ -1,35 +1,28 @@
 /*
  * mandr024_lab1_part1.c
  *
- * Created: 7/30/2018 1:41:59 PM
+ * Created: 7/30/2018 2:32:59 PM
  * Author : ucrcse
  */ 
 
 #include <avr/io.h>
 
+
 int main(void)
 {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs,
-	// initialize to 0s
-	unsigned char tmpB = 0x00;     // Temporary variable to hold the value of B
-	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
-	while(1)
-	{
-		// (1) Read input
-		tmpA = PINA & 0x01;
-		// (2) Perform computation
-		// if PA0 is 1, set PB1PB0=01, else=10
-		if (tmpA == 0x01) { // True if PA0 is 1
-			tmpB = (tmpB & 0xFC) | 0x01; // Sets tmpB to bbbbbb01
-			// (clear rightmost 2 bits, then set to 01)
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+		if((PINA & 0x03) == 0x01)
+		{
+			PORTB = 0x01;
 		}
-		else {
-			tmpB = (tmpB & 0xFC) | 0x02; // Sets tmpB to bbbbbb10
-			// (clear rightmost 2 bits, then set to 10)
-		}
-		// (3) Write output
-		PORTB = tmpB;
+		
+		else
+			PORTB = 0x00;
 	}
-	return 0;
 }
+
