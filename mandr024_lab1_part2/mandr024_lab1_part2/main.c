c732e2239d9a61e742295992635fd35d60078a8f
miguelandrade4 <miguel.jr.andrade49@gmail.com>
2018-07-30 16:07:49 -0700

    Updated Problem 2

--- mandr024_lab1_part2/mandr024_lab1_part2/main.c
+++ mandr024_lab1_part2/mandr024_lab1_part2/main.c
@@ -14,29 +14,25 @@ int main(void)
     /* Replace with your application code */
     while (1) 
     {
-		tempA = (PINA & 0x07);
+		tempA = (PINA & 0x0F);
 		
-		if((tempA == 0x01) || (tempA == 0x02) || (tempA == 0x04) || (tempA == 0x08))
+		if ((tempA == 0x01) || (tempA == 0x02) || (tempA == 0x04) || (tempA == 0x08))
 		{
 			PORTC = 0x03;
 		}
-		
 		else if((tempA == 0x03) || (tempA == 0x05) || (tempA == 0x06) || (tempA == 0x09) || (tempA == 0x0A) || (tempA == 0x0C))
 		{
 			PORTC = 0x02;
 		}
-		
 		else if((tempA == 0x07) || (tempA == 0x0B) || (tempA == 0x0D) || (tempA == 0x0E))
 		{
 			PORTC = 0x01;
 		}
-		
 		else if((tempA == 0x0F))
 		{
 			PORTC = 0x00;
 		}
-		
-		else if((tempA == 0x00))
+		else
 		{
 			PORTC = 0x04;
 		}
