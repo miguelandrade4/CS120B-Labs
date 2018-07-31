303069cb23a1159c46577d4f3634d9494db56b3c
miguelandrade4 <miguel.jr.andrade49@gmail.com>
2018-07-31 14:26:50 -0700

    Challenge COmplete

--- mandr024_lab1_Challenge/mandr024_lab1_Challenge/main.c
+++ mandr024_lab1_Challenge/mandr024_lab1_Challenge/main.c
@@ -6,14 +6,20 @@
  */ 
 
 #include <avr/io.h>
-
+#define range 84
 
 int main(void)
 {
-	signed char tempA;
-	signed char tempB;
-	signed char tempC;
-	unsigned char tempNum;
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRB = 0x00; PORTB = 0xFF;
+	DDRC = 0x00; PORTC = 0xFF; // Configure port C's 8 pins as inputs
+	DDRD = 0xFF; PORTD = 0x00;
+	
+	unsigned char tempA;
+	unsigned char tempB;
+	unsigned char tempC;
+	unsigned short tempNum;
+	unsigned short tempNumTwo;
     /* Replace with your application code */
     while (1) 
     {
@@ -21,13 +27,15 @@ int main(void)
 		tempB = PINB;
 		tempC = PINC;
 		
+		tempNumTwo = tempA + tempB + tempC;
+		
 		tempNum = tempA - tempC;
 		
 		tempNum = abs(tempNum);
 		
-		if( (PINA == 140) || (PINB == 140) || (PINC == 140))
+		if( tempNumTwo >= 140)
 		{
-			if(tempNum == 80)
+			if(tempNum >= 80)
 			{
 				PORTD = 0x02;
 			}
@@ -39,6 +47,30 @@ int main(void)
 				
 		}
 		
+		if( tempNumTwo > 255 && tempNumTwo < 339) //range of 84 for 1 bit each estimate quick and easy
+		{
+			PORTD = 0x04;
+		}
+		else if( tempNumTwo > 339 && tempNumTwo < 423)
+		{
+			PORTD = 0x08;
+		}
+		else if( tempNumTwo > 423 && tempNumTwo < 507)
+		{
+			PORTD = 0x10;
+		}
+		else if( tempNumTwo > 507 && tempNumTwo < 591)
+		{
+			PORTD = 0x20;
+		}
+		else if( tempNumTwo > 591 && tempNumTwo < 675)
+		{
+			PORTD = 0x40;
+		}
+		else if( tempNumTwo > 675 && tempNumTwo < 765) // not 84 range but to accommodate
+		{
+			PORTD = 0x80;
+		}
     }
 }
 
